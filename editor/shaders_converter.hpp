10c58fb9f2d57598e38dfa5a84e199be7b8857df
iamdramaticleo <iamdramaticleo@gmail.com>
2025-10-13 22:52:55 +0300

    Make `ShadersConverter` non-instantiable by deleting the default constructor

--- editor/shaders_converter.hpp
+++ editor/shaders_converter.hpp
@@ -7,5 +7,7 @@ namespace editor
     public:
         static auto convert_each(const std::filesystem::path& input, const std::filesystem::path& output) -> void;
         static auto convert_file(const std::filesystem::path& input, const std::filesystem::path& output) -> void;
+
+        ShadersConverter() = delete;
     };
 }
