3209343ec638cce079dc8968c381a6a76d6bfccc
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-07 23:38:37 +0200

    Reorder `ShadersConverter` constructor declaration for better readability

--- tools/shaders_converter.hpp
+++ tools/shaders_converter.hpp
@@ -5,9 +5,9 @@ namespace tools
     class ShadersConverter
     {
     public:
+        ShadersConverter() = delete;
+
         static auto convert_each(const std::filesystem::path& input, const std::filesystem::path& output) -> void;
         static auto convert_file(const std::filesystem::path& input, const std::filesystem::path& output) -> void;
-
-        ShadersConverter() = delete;
     };
 }
