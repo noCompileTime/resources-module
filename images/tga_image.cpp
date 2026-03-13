af483100b23810dddaa236033abd1e3ef9325887
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-13 14:32:50 +0200

    Refactor `TgaImage::load` to improve readability and fix spacing issues.

--- images/tga_image.cpp
+++ images/tga_image.cpp
@@ -5,27 +5,28 @@ namespace images
 {
     auto TgaImage::load(const std::filesystem::path& path) -> core::image
     {
-      assert(is_regular_file(path)); // TODO handle different if there is no file
+        tga_header header;
+
+        assert(is_regular_file(path)); // TODO handle different if there is no file
 
         std::ifstream stream(path, std::ios::in | std::ios::binary);
                assert(stream);
 
-        tga_header header;
-
         stream.read(reinterpret_cast<char*>(&header), sizeof(tga_header));
-                            assert(stream.gcount() == sizeof(tga_header)); // TODO handle different if there is a corrupted tga
+
+        assert(stream.gcount() == sizeof(tga_header)); // TODO handle different if there is a corrupted tga
 
         const auto channels = header.pixel_depth / 8;
         const auto     size = header.width * header.height * channels;
+        std::vector<uint8_t> content(size);
 
-                       std::vector<uint8_t> content(size);
-        stream.read(reinterpret_cast<char*>(content.data()),  size);
-                                    assert(stream.gcount() == size); // TODO handle different if there is a corrupted tga
+        stream.read(reinterpret_cast<char*>(content.data()), size);
+
+        assert(stream.gcount() == static_cast<std::streamsize>(size)); // TODO handle different if there is a corrupted tga
 
         for (auto i = 0; i < size; i += channels)
         {
-            std::swap(content[i],
-                      content[i + 2]);
+            std::swap(content[i], content[i + 2]);
         }
 
         return
