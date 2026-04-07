429139f38d04908248fd8707fc821011ce748daa
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-07 16:16:59 +0300

    Add TODO comments for future enhancements in core modules
    Inserted comments to highlight areas requiring further development or refactoring. These include switching to a unified logger and considering alternative data structures for improved searchability.

--- core/model.hpp
+++ core/model.hpp
@@ -6,6 +6,6 @@ namespace core
 {
     struct model
     {
-        std::vector<geometry<vertex::type::model>> geometries;
+        std::vector<geometry<vertex::type::model>> geometries; // TODO here to have a map? or a flat map? to seach stuff by name?
     };
 }
