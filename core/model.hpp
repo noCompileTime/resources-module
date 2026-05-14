a1c541f756ba8cbaafd3fbc219fd020b6f611a70
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-14 22:47:31 +0300

    Remove redundant blank line in `create_circle` method

--- core/model.hpp
+++ core/model.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "core/geometry.hpp"
+#include "core/vertex/geometry.hpp"
 
 namespace core
 {
     struct model
     {
-        std::vector<geometry<vertex::type::model>> geometries; // TODO here to have a map? or a flat map? to seach stuff by name?
+        std::vector<vertex::geometry<vertex::type::model>> geometries; // TODO here to have a map? or a flat map? to seach stuff by name?
     };
 }
--- models/obj_model.cpp
+++ models/obj_model.cpp
@@ -12,7 +12,7 @@ namespace models
         {
             uint32_t index = 0;
 
-            core::geometry<core::vertex::type::model> geometry;
+            core::vertex::geometry<core::vertex::type::model> geometry;
 
             // TODO use geometry reserve here after checking the number of reallocation
 
