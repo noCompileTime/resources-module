0ec4808f947dfff786bc4bd4beabdc961bfb8976
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-25 11:43:16 +0300

    Refactor geometry classes and methods for consistency
    Renamed geometry classes (e.g., `Sprites` to `Sprite`, `Primitives` to `Primitive`) and updated method names to improve clarity and alignment with naming conventions. Adjusted includes and usage in relevant files to reflect these changes.

--- core/image.hpp
+++ core/image.hpp
@@ -2,7 +2,7 @@
 
 namespace core
 {
-    struct image
+    struct image // TODO can this go in common? should go inside common?
     {
         int32_t width;
         int32_t height;
--- core/model.hpp
+++ core/model.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "geometry/types.hpp"
+#include "geometry/geometry_type.hpp"
 
 namespace core
 {
-    struct model
+    struct model // TODO can this go in common? should go inside common?
     {
         std::vector<geometry::model> geometries; // TODO here to have a map? or a flat map? to seach stuff by name?
     };
