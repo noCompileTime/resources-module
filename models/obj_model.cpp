25e023a15f48dc66813d3091ead84144c2c9b5a3
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-01 20:16:52 +0300

    Refactor namespaces for consistency under `core` root.
    Renamed `geometry`, `vertex`, `primitive`, `data`, and other namespaces to be under the `core` namespace hierarchy for improved organization and clarity. Updated relevant file references and code usage accordingly across modules.

--- models/obj_model.cpp
+++ models/obj_model.cpp
@@ -12,7 +12,7 @@ namespace models
         {
             uint32_t index = 0;
 
-            geometry::model geometry;
+            core::geometry::model geometry;
 
             // TODO use geometry reserve here after checking the number of reallocation
 
